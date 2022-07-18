41455aca86959fa4085f8c2d426516b819f7f7bb
Lincoln-developer <angufibolinc@gmail.com>
2022-07-18 12:20:10 +0300

    added featured products and image folder in public folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/">amazona</a>      
+        <a href="/">amazona</a>
       </header>
       <main>
-        list products 
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-infor">
+                <a href={`products/${product.slug}`}>
+                <p>{product.name}</p>
+                <p><strong>${product.price}</strong></p>
+                </a>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
