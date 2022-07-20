16e088d806cbf014bcf1f1654aba37188856c732
Lincoln-developer <angufibolinc@gmail.com>
2022-07-20 13:00:00 +0300

    added user authentication

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,17 +1,16 @@
-import express from "express";
-import data from "../data.js";
-import Product from "../models/productModel.js";
-import User from "../models/userModel.js";
+import express from 'express';
+import data from '../data.js';
+import Product from '../models/productModel.js';
+import User from '../models/userModel.js';
 
 const seedRoute = express.Router();
 
-seedRoute.get('/', async(req,res)=>{
-    await Product.remove({});
-    const createdProducts = await Product.insertMany(data.products);
-    await User.remove({});
-    const createdUsers = await User.insertMany(data.users);
-    res.send({createdProducts, createdUsers})
- 
+seedRoute.get('/', async (req, res) => {
+  await Product.remove({});
+  const createdProducts = await Product.insertMany(data.products);
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seedRoute;
